494b8c1b8bc5f56217cbcdf7e97488dfac0976d0
Senjars <jaroslawsendecki1@gmail.com>
2025-11-23 21:14:16 +0100

    First solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,7 +1,20 @@
 package mate.academy;
 
+import mate.academy.dao.MovieService;
+import mate.academy.lib.Injector;
+import mate.academy.model.Movie;
+
 public class Main {
     public static void main(String[] args) {
+
+        Injector injector = Injector.getInstance("mate.academy");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+
+        Movie movie = new Movie(1L, "Harry Potter and the Order of the Phoenix",
+                "Harry joins the Order of the Phoenix");
+
+        movieService.add(movie);
+        movieService.get(1L);
 
     }
 }
---
+++ src/main/java/mate/academy/dao/MovieDaoImpl.java
@@ -0,0 +1,39 @@
+package mate.academy.dao;
+
+import java.util.Optional;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+
+    @Override
+    public Movie add(Movie movie) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(movie);
+            transaction.commit();
+            session.close();
+            return movie;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add movie: " + movie);
+        }
+    }
+
+    @Override
+    public Optional<Movie> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return Optional.ofNullable(session.find(Movie.class, id));
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get movie: " + id + "\n" + e.getMessage());
+        }
+    }
+}
