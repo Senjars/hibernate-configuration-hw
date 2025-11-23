b52fafc76a5426c76594e5db60309b0de7638f9b
Senjars <jaroslawsendecki1@gmail.com>
2025-11-23 21:19:52 +0100

    First solution

--- src/main/java/mate/academy/model/Movie.java
+++ src/main/java/mate/academy/model/Movie.java
@@ -1,7 +1,15 @@
 package mate.academy.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Movie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
@@ -10,6 +18,9 @@ public class Movie {
         this.id = id;
         this.title = title;
         this.description = description;
+    }
+
+    public Movie() {
     }
 
     public String getDescription() {
